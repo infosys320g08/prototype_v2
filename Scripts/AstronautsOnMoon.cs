b7ff3c3e83b3e764db72fc5797654666e24dfbfa
infosys320g08 <42130860+infosys320g08@users.noreply.github.com>
2018-10-01 16:18:25 +1300

    Add files via upload

---
+++ Scripts/AstronautsOnMoon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstronautsOnMoon
+{
+    //List all the columns of your table here in the same format as below.
+    public int AstronautID { get; set; }
+    public string AstronautName { get; set; }
+    public string Mission { get; set; }
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+}
---
+++ Scripts/Chair.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chair
+{
+    //List all the columns of your table here in the same format as below.
+    public int SeatID { get; set; }
+    public string SeatNumber { get; set; }
+    public string Class { get; set; }
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+}
